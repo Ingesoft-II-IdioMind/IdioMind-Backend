37d69f65b293d531f2af09f78fdf7ca6cd1e02ea
unknown <amoralesco@unal.edu.co>
2024-03-26 18:29:45 -0500

    endpoint users

--- Accounts/models.py
+++ Accounts/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import BaseUserManager,AbstractBaseUser, Permiss
 
 # Create your models here.
 class UserAccountManager(BaseUserManager):
-    def create_user(self,first_name,last_name,email,password=None, **kwargs):
+    def create_user(self,email,password=None, **kwargs):
         if not email:
             raise ValueError('Users must have an email address')
         
@@ -22,7 +22,7 @@ class UserAccountManager(BaseUserManager):
 
         return user
 
-    def create_user(self,email,password=None,**kwargs):
+    def create_superuser(self,email,password=None,**kwargs):
         user = self.create_user(email,password=password,**kwargs)
         user.is_staff = True
         user.is_superuser = True
