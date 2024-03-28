858ecca36cdab52912ce2eeb3548c216b9a7a7a9
caeamayaan <caeamayaan@unal.edu.co>
2024-03-27 20:51:45 -0500

    Conexión corregida railway 2

--- idiomind/settings.py
+++ idiomind/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = getenv('DJANGO_SECRET_KEY',get_random_secret_key())
 DEBUG = getenv('DEBUG','False') == 'True'
 
 
-ALLOWED_HOSTS=getenv('DJANGO_ALLOWED_HOSTS','127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS=getenv('DJANGO_ALLOWED_HOSTS','127.0.0.1,localhost,idiomind-backend-production.up.railway.app').split(',')
 
 
 
