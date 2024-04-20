2afba6c9a064e973c2115bc592533ba3425cf568
unknown <amoralesco@unal.edu.co>
2024-04-20 13:28:03 -0500

    requirements generativeai remove

--- Documents/utils.py
+++ Documents/utils.py
@@ -1,36 +1,40 @@
 from idiomind import settings
-import google.generativeai as genai
 from firebase_admin import storage
 import json
+import requests
 
 def translate_word(word, language, sentence=None):
-    genai.configure(api_key=settings.API_KEY)
-    model = genai.GenerativeModel('gemini-pro')
+    api_key=settings.API_KEY
+    
 
     if sentence:
         prompt = "You are an assistant providing responses in JSON format. Provide the definition, description in its context, and three examples of the word " + word + " and its use in the following sentence: '" + sentence + "'. Follow this JSON format: {\"translation\":\"<your word translation to" + language + ">\", \"definition\":\"<definition in the original language>\", \"description\":\"<description in the original language>\", \"examples\":[\"<Example1>\",\"<Example2>\",\"<Example3>\"]}. Your responses (except the translation) must be in the language of the word " + word + "."
     else:
         prompt = "You are an assistant providing responses in JSON format. Provide the definition and three examples of the word " + word + ". Follow this JSON format: {\"translation\":\"<your word translation to" + language + ">\", \"definition\":\"<definition in the original language>\", \"examples\":[\"<Example1>\",\"<Example2>\",\"<Example3>\"]}. Your responses (except the translation) must be in the language of the word " + word + "."
-   
-    response = model.generate_content(prompt)
-    json_content = str(response.text)
-    
-    # Remove the markdown code block syntax (json and )
-    json_content = json_content.replace('json\n', '').replace('\n', '')
-    json_content = json.loads(json_content)
-    
-    # Asegurarse de que todos los campos estén presentes en el objeto JSON
-    translation = json_content.get('translation', None)
-    definition = json_content.get('definition', None)
-    description = json_content.get('description', None)
-    examples = json_content.get('examples', None)
-    
-    return {
-        'translation': translation,
-        'definition': definition,
-        'description': description or 'None',  # Si no hay descripción, establecer en 'None'
-        'examples': examples,
+
+    endpoint = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent"
+    headers = {
+        "Content-Type": "application/json"
     }
+    data = {
+        "contents": [{
+            "parts": [{
+                "text": prompt
+            }]
+        }]
+    }
+    
+    response = requests.post(endpoint, headers=headers, params={"key": api_key}, json=data)
+    json_content = response.json()
+
+    # Extracting information from the response
+    result = json_content.get('candidates', [{}])[0].get('content', {}).get('parts', [{}])[0].get('text', None)
+    result_dict = json.loads(result)
+    translation = result_dict.get('translation')
+    definition = result_dict.get('definition')
+    examples = result_dict.get('examples')
+    return translation,definition, examples
+
 
 def subir_pdf(pdf_file):
     try:
