3d57872aa76a2ebe2ca48d567ec8679830f8a37e
unknown <amoralesco@unal.edu.co>
2024-04-24 20:45:40 -0500

    add host url

--- idiomind/settings.py
+++ idiomind/settings.py
@@ -44,7 +44,7 @@ storage_client = storage.bucket(bucket_name)
 DEBUG = getenv('DEBUG','False') == 'True'
 
 
-ALLOWED_HOSTS=getenv('DJANGO_ALLOWED_HOSTS','127.0.0.1,localhost,idiomind-backend-production.up.railway.app,idiomind-frontend-git-h09-jeramirezcas-projects.vercel.app').split(',')
+ALLOWED_HOSTS=getenv('DJANGO_ALLOWED_HOSTS','127.0.0.1,localhost,idiomind-backend-production.up.railway.app,idiomind-frontend.vercel.app,idiomind-frontend-git-h09-jeramirezcas-projects.vercel.app').split(',')
 
 
 
@@ -187,7 +187,7 @@ DJOSER = {
     'USER_CREATE_PASSWORD_RETYPE': True,
     'PASSWORD_RESET_CONFIRM_RETYPE': True,
     'TOKEN_MODEL': None,
-    'SOCIAL_AUTH_ALLOWED_REDIRECT_URIS': getenv('REDIRECT_URLS','https://idiomind-frontend-git-h09-jeramirezcas-projects.vercel.app').split(',')
+    'SOCIAL_AUTH_ALLOWED_REDIRECT_URIS': getenv('REDIRECT_URLS','http://127.0.0.1:3000,https://idiomind-frontend-git-h09-jeramirezcas-projects.vercel.app,https://idiomind-frontend.vercel.app').split(',')
 }
 
 AUTH_COOKIE = 'access'
@@ -207,7 +207,7 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = [
 ]
 SOCIAL_AUTH_GOOGLE_OAUTH2_EXTRA_DATA = ['first_name', 'last_name']
 
-CORS_ALLOWED_ORIGINS = getenv('CORS_ALLOWED_ORIGINS','http://localhost:3000,http://127.0.0.1:3000,https://idiomind-frontend-git-h09-jeramirezcas-projects.vercel.app,https://idiomind-backend-production.up.railway.app').split(',')
+CORS_ALLOWED_ORIGINS = getenv('CORS_ALLOWED_ORIGINS','http://localhost:3000,http://127.0.0.1:3000,https://idiomind-frontend-git-h09-jeramirezcas-projects.vercel.app,https://idiomind-frontend.vercel.app/,https://idiomind-backend-production.up.railway.app').split(',')
 
 CORS_ALLOW_CREDENTIALS = True
 
