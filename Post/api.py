7e6c53d46b1f752019c6ef370d104c8a7f505b4a
unknown <amoralesco@unal.edu.co>
2024-05-03 16:38:29 -0500

    edit post restringed to only text, not image

--- Post/api.py
+++ Post/api.py
@@ -1,8 +1,8 @@
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer,PostUpdateSerializer
 from rest_framework import viewsets
 from rest_framework.exceptions import PermissionDenied
-from rest_framework.response import Response
+
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -10,8 +10,14 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def get_serializer_class(self):
         # Usar el serializer PostSerializer solo para solicitudes de escritura si el usuario es un superusuario
-        if self.request.method in ['POST', 'PUT', 'PATCH'] and not self.request.user.is_superuser:
+        if self.request.method in ['POST'] and not self.request.user.is_superuser:
             raise PermissionDenied("Solo los superusuarios pueden crear o actualizar posts.")
+        if self.request.method in  ['PUT', 'PATCH'] and not self.request.user.is_superuser:
+            raise PermissionDenied("Solo los superusuarios pueden crear o actualizar posts.")
+        if self.request.method in ['POST']:
+            return PostSerializer
+        if self.request.method in  ['PUT', 'PATCH']:
+            return PostUpdateSerializer
         return PostSerializer
     
 
--- Post/serializers.py
+++ Post/serializers.py
@@ -24,3 +24,16 @@ class PostSerializer(serializers.ModelSerializer):
             return super().update(instance, validated_data)
         else:
             raise serializers.ValidationError("Solo los superusuarios pueden actualizar posts.")
+        
+class PostUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('id', 'Titulo', 'Fecha_publicacion','Autor','Imagen','Contenido')
+        read_only_fields = ('id','Fecha_publicacion','Imagen')
+
+
+    def update(self, instance, validated_data):
+        if self.context['request'].user.is_superuser:
+            return super().update(instance, validated_data)
+        else:
+            raise serializers.ValidationError("Solo los superusuarios pueden actualizar posts.")
