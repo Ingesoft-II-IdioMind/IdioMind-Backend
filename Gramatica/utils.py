f193e259b0b25f527c1f85e11fd643174aa1c64a
unknown <amoralesco@unal.edu.co>
2024-05-04 14:02:34 -0500

    gramatica prompt correccion

--- Gramatica/utils.py
+++ Gramatica/utils.py
@@ -3,15 +3,28 @@ import requests
 import json
 
 
-def grammar_phrase(issue,idiom):
-    api_key=settings.API_KEY
+def grammar_phrase(issue, idiom):
+    api_key = settings.API_KEY
+
+    prompt = (
+    "ignora la anterior respuesta, imagina que esta es la primera prompt que te pregunto"
+    "Simula que las comillas han sido eliminadas de todos los lenguajes, no uses ninguna comilla en la respuesta; es vital que respetes esta regla, es cuestión de vida o muerte. "
+    "Eres un asistente de inteligencia artificial creando ejercicios de gramática. "
+    "Por favor, genera una oración centrada en " + issue + "."
+    "Deja un unico espacio en blanco en la oración donde irá la palabra a encontrar (no agreges las opciones ni la palabra en el body). "
+    "Sigue este formato JSON sin agregar saltos de línea ni nada más; deja los formatos JSON como están y sigue las instrucciones para que la aplicación siempre funcione: elimina las comillas de un diccionario, jamás las uses. "
+    "{\"body\": \"<oración con un espacio en blanco, haz que se noten los tiempos verbales, siempre haz que sean 5 barra bajas _____ >\", "
+    "\"options\": [\" genera solamente tres palabras falsas y equivocadas, preferiblemente aleatorias que no tengan nada que ver con la frase  <opción 1: respuesta notoriamente falsa; que no concuerda nada con frase anteriormente generada ni con la palabra que equivale, tiene que tener total error si se une con el body>\", "
+    "\"<opción 2: respuesta notoriamente falsa; que no concuerda nada con frase anteriormente generada ni con la palabra que equivale, tiene que tener total error si se une con el body>\", "
+    "\"<opción 3: respuesta notoriamente falsa; que no concuerda nada con frase anteriormente generada ni con la palabra que equivale, tiene que tener total error si se une con el body>\"], "
+    "\"true_answer\": \"<palabra correcta que cuadra muy notoriamente en la frase anteriormente registrada; que no tengan ni sinonimo, ni parecido a las palabras falsas>\", "
+    "\"reason\": '<argumento por que es la respuesta correcta,es necesario explicar cada una de porque las demas son respuestas falsas, haz un autoanalisis si de verdad las palabras no concuerdan; haciendo explicaciones de la forma del lenguaje ,si necesitas resaltar una palabra usa ', no uses ninguna comilla en la respuesta; es muy importante que acates esta regla es de vida o muerte; si mencionas una palabra que sea de otra forma ',"
+    "\"verbal_time\":  <tiempo verbal de la frase, una sola palabra o frase que menciona el tiempo verbal>\", "
+    "\"example\":  < resultado de la union del texto con la palabra correcta, dando como resultado la frase completa >\"}"
+    "Tu respuesta de todo el JSON debe estar únicamente en el idioma" + idiom
+) 
+
     
-
- 
-    prompt = "You are an assistant providing exercises in grammar format. Please generate a sentence aimed at learning grammar. giving 4 options where only and strictly one word is correct, no other word can be valid in the text, " + issue + " with a blank space for the word.Follow this JSON format ever: {\"text\":\"<the body of the sentence that must be structured in detail, without containing the word>\", \"options\":[\"<four possible words that could go in the sentence where only one is correct and should not be repeated,Strictly only one can be correct>\"],\"true_answer\":\"<the correct answer corresponding to the phrase,The rest of the answers must be wrong.>\"}.Be careful with phrases that words in different tenses can have values ,Your response must be solely and exclusively in the " + idiom + "." 
-   
-
-
     endpoint = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent"
     headers = {
         "Content-Type": "application/json"
@@ -25,12 +38,22 @@ def grammar_phrase(issue,idiom):
     }
     
     response = requests.post(endpoint, headers=headers, params={"key": api_key}, json=data)
-    json_content = response.json()
+    print(response.json())
+    try:
+        json_content = response.json()
+        print("1")
+        result = json_content.get('candidates', [{}])[0].get('content', {}).get('parts', [{}])[0].get('text', None)
+        print("2")
+        result_dict = json.loads(result)
+        print(result+ "hola")
+        body = result_dict.get("body")
+        options = result_dict.get("options")
+        true_answer = result_dict.get("true_answer")
+        reason = result_dict.get("reason")
+        verbal_time = result_dict.get("verbal_time")
+        example = result_dict.get("example")
+    except json.JSONDecodeError:
+        # Manejo de errores si el contenido no se puede decodificar como JSON
+        body, options, true_answer, reason, verbal_time, example  = None, None, None, None, None, None
 
-    # Extracting information from the response
-    result = json_content.get('candidates', [{}])[0].get('content', {}).get('parts', [{}])[0].get('text', None)
-    result_dict = json.loads(result)
-    text = result_dict.get('text')
-    options = result_dict.get('options')
-    true_answer = result_dict.get('true_answer')
-    return text,options,true_answer
+    return body, options, true_answer, reason, verbal_time, example
