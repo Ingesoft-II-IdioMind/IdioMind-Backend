7c4d6e330cde9797a922920c743e4829c3b5b78f
unknown <amoralesco@unal.edu.co>
2024-05-13 10:45:48 -0500

    staticfiles

--- idiomind/settings.py
+++ idiomind/settings.py
@@ -165,10 +165,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
 
+# Directorios adicionales donde Django buscará archivos estáticos
+STATICFILES_DIRS = [
+    path.join(BASE_DIR, 'static'),
+]
 
-
+# Ubicación donde se recopilarán los archivos estáticos
+STATIC_ROOT = path.join(BASE_DIR, 'staticfiles')
 
 
 
