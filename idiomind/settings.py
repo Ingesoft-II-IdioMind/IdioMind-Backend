107a623cab67275d601df05d0101aa46fb67ee79
unknown <amoralesco@unal.edu.co>
2024-05-20 19:07:21 -0500

    static

--- idiomind/settings.py
+++ idiomind/settings.py
@@ -78,6 +78,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'idiomind.urls'
@@ -158,10 +159,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR  / 'static'
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR  / 'media/'
+STATIC_URL = 'staticfiles/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATIC_ROOT = path.join(BASE_DIR, 'staticfiles')
 
 
 
