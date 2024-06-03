eb17841576577d9df3bf6b6ee22c5b253379eb89
camayae <111199612+camayae@users.noreply.github.com>
2024-06-03 15:59:00 -0500

    UpdateSettings2.py

--- idiomind/settings.py
+++ idiomind/settings.py
@@ -21,7 +21,7 @@ import stripe
 pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(_file_).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 dotenv_file = BASE_DIR / '.env.local'
 
